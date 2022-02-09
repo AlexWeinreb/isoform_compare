ee398733cf2ac0d0d75a98c3d0761381bcb1ca88
AlexWeinreb <alexis.weinreb@yale.edu>
2022-02-09 13:27:35 -0500

    Commit initial working app.

---
+++ R/utils.R
@@ -0,0 +1,133 @@
+
+# add the % sign on the axis (for values already between 1-100)
+pct <- \(...) scales::number(..., suffix = "%")
+
+
+# Convert vector of any gene format into WBGene ID
+convert_to_wb_id <- function(gene_id_or_name, gids){
+  is_wb_id <- str_detect(gene_id_or_name, "^WBGene[\\d]{8}$")
+  is_seq_name <- str_detect(gene_id_or_name, "^[A-Z0-9.]+$")
+  is_symbol <- str_detect(gene_id_or_name, "^[a-z]{3,4}-[\\d]{1,4}")
+  
+  # Check
+  nb_class <- (is_wb_id + is_seq_name + is_symbol)
+  multi_class <- gene_id_or_name[nb_class > 1]
+  unclassed <- gene_id_or_name[nb_class == 0]
+  
+  if(length(multi_class) != 0L){
+    warning("Genes were misclassified during name conversion: ", multi_class)
+  }
+  if(length(unclassed) != 0L){
+    warning("Genes were not classified during name conversion: ", unclassed)
+  }
+  
+  # create out
+  out_gene_id <- character(length(gene_id_or_name))
+  out_gene_id[is_wb_id] <- gene_id_or_name[is_wb_id]
+  out_gene_id[is_seq_name] <- wb_seq2id(gene_id_or_name[is_seq_name], gids, warn_missing = TRUE)
+  out_gene_id[is_symbol] <- s2i(gene_id_or_name[is_symbol], gids, warn_missing = TRUE)
+  
+  out_gene_id
+}
+
+# split gene or neuron list into vector
+split_text_to_vector <- function(text){
+  str_split(text,
+            pattern = "[[:space:],;]")[[1]] |>
+    stringi::stri_remove_empty()
+}
+
+# ensure we have a list of valid neurons
+validate_neurons <- function(neurs, neurons_table){
+  
+  # replace synonyms
+  neurs <- recode(neurs,
+                  SENS="SENSORY",
+                  INTER="INTERNEURON",
+                  INTERNEURONS="INTERNEURON",
+                  MOTORNEURON="MOTOR",
+                  MOTORNEURONS="MOTOR",
+                  MOTONEURON="MOTOR",
+                  MOTONEURONS="MOTOR",
+                  PHARYNX="PHARYNGEAL",
+                  PHA="PHARYNGEAL",
+                  CILIA="CILIATED",
+                  CHOLINERGIC="ACH",
+                  ACHERGIC="ACH",
+                  ACETYLCHOLINERGIC="ACH",
+                  ACETYLCHOLINE="ACH",
+                  CHO="ACH",
+                  SEROTONERGIC="SEROTONIN",
+                  SEROTONINERGIC="SEROTONIN",
+                  `5-HT`="SEROTONIN",
+                  `5HT`="SEROTONIN",
+                  GLUTAMATERGIC="GLUTAMATE",
+                  GLU="GLUTAMATE",
+                  GLUERGIC="GLUTAMATE",
+                  GLUTA="GLUTAMATE",
+                  GABAERGIC="GABA",
+                  OCTO="OCTOPAMINE",
+                  OCTOPAMINERGIC="OCTOPAMINE")
+  
+  
+  if(length(neurs) == 1L && str_to_upper(neurs) == "ALL"){
+    return(neurons_table$Neuron_type)
+  }
+  
+  
+  # Subcategories
+  if(any(neurs == "SENSORY")){
+    neurs <- setdiff(neurs, "SENSORY") |>
+      c(neurons_table$Neuron_type[str_detect(neurons_table$Modality, "sensory")])
+  }
+  if(any(neurs == "INTERNEURON")){
+    neurs <- setdiff(neurs, "INTERNEURON") |>
+      c(neurons_table$Neuron_type[str_detect(neurons_table$Modality, "interneuron")])
+  }
+  if(any(neurs == "MOTOR")){
+    neurs <- setdiff(neurs, "MOTOR") |>
+      c(neurons_table$Neuron_type[str_detect(neurons_table$Modality, "motor")])
+  }
+  if(any(neurs == "PHARYNGEAL")){
+    neurs <- setdiff(neurs, "PHARYNGEAL") |>
+      c(neurons_table$Neuron_type[str_detect(neurons_table$Modality, "pharyngeal")])
+  }
+  if(any(neurs == "CILIATED")){
+    neurs <- setdiff(neurs, "CILIATED") |>
+      c(neurons_table$Neuron_type[neurons_table$Ciliated == "yes"])
+  }
+  if(any(neurs == "ACH")){
+    neurs <- setdiff(neurs, "ACH") |>
+      c(neurons_table$Neuron_type[str_detect(neurons_table$Neurotransmitter, "ach")])
+  }
+  if(any(neurs == "SEROTONIN")){
+    neurs <- setdiff(neurs, "SEROTONIN") |>
+      c(neurons_table$Neuron_type[str_detect(neurons_table$Neurotransmitter, "serotonin")])
+  }
+  if(any(neurs == "GLUTAMATE")){
+    neurs <- setdiff(neurs, "GLUTAMATE") |>
+      c(neurons_table$Neuron_type[str_detect(neurons_table$Neurotransmitter, "glutamate")])
+  }
+  if(any(neurs == "GABA")){
+    neurs <- setdiff(neurs, "GABA") |>
+      c(neurons_table$Neuron_type[str_detect(neurons_table$Neurotransmitter, "gaba")])
+  }
+  if(any(neurs == "OCTOPAMINE")){
+    neurs <- setdiff(neurs, "OCTOPAMINE") |>
+      c(neurons_table$Neuron_type[str_detect(neurons_table$Neurotransmitter, "octopamine")])
+  }
+  
+  
+  
+  
+  
+  
+  # Remove unknown neurons
+  unknown_neurs <- setdiff(neurs, neurons_table$Neuron_type)
+  
+  if(length(unknown_neurs) != 0L){
+    warning("Neuron name not recognized: ", unknown_neurs)
+  }
+  
+  setdiff(neurs, unknown_neurs)
+}
---
+++ app.R
@@ -0,0 +1,285 @@
+
+
+
+# Initializations ----
+
+#~ Packages ----
+library(shiny)
+library(plotly)
+library(tidyverse)
+library(InteractiveComplexHeatmap)
+
+library(wbData)
+gids <- wb_load_gene_ids(281)
+
+#~ Data ----
+tx_long <- read_tsv("data/t_exp.tsv",
+                    col_types = cols(transcript_id = col_character(),
+                                     gene_id = col_character(),
+                                     sample_id = col_character(),
+                                     neuron_id = col_character(),
+                                     TPM = col_double()))
+stop_for_problems(tx_long)
+
+neurons_table <- read_csv("data/neuron_properties.csv",
+                          col_types = "cccc") |>
+    filter(Neuron_type %in% as.character(unique(tx_long$neuron_id))) |>
+    mutate(across(Modality:Neurotransmitter, str_to_lower))
+stop_for_problems(neurons_table)
+
+
+accepted_color_scales <- tribble(~shortcode, ~ggplot,
+                                 'viridis' , viridis::scale_fill_viridis(discrete = TRUE),
+                                 'ggsci_d3_cat20', ggsci::scale_color_d3("category20"),
+                                 'ggsci_npg', ggsci::scale_fill_npg(),
+                                 'iwanthue', hues::scale_fill_iwanthue())
+
+
+
+#~ Functions ----
+source("R/utils.R")
+
+
+
+# Define UI ----
+ui <- fluidPage(
+    
+    # Application title
+    titlePanel("Transcript-level quantification"),
+    sidebarLayout(
+        sidebarPanel(
+            textInput(inputId = "gene_id_or_name",
+                      label = "Gene:",
+                      value = "ric-4"),
+            textInput(inputId = "neurons",
+                      label = "Neurons:",
+                      value = "ALL"),
+            checkboxInput("plotIndividualSamples", "Plot individual samples"),
+            checkboxInput("log_scale", "Use log scale for TPM"),
+            selectInput("useColorScale", "Color scale", accepted_color_scales$shortcode)
+        ),
+        mainPanel(
+            tabsetPanel(
+                tabPanel("Single gene",
+                         
+                         
+                         
+                         
+                         conditionalPanel('input.plotIndividualSamples == 0',
+                                          plotlyOutput("tx_neuron_proportions")),
+                         conditionalPanel('input.plotIndividualSamples == 0',
+                                          plotlyOutput("tx_neuron_tpm")),
+                         conditionalPanel('input.plotIndividualSamples == 1',
+                                          plotlyOutput("tx_samples_proportions")),
+                         conditionalPanel('input.plotIndividualSamples == 1',
+                                          plotlyOutput("tx_samples_tpm")),
+                         width = 3
+                ),
+                
+                tabPanel("Heatmap of transcript expression",
+                         
+                         # plotOutput("heatmapCplx"),
+                         plotOutput("heatmapPhmp", width = "100%")
+                         
+                )
+            )
+        )
+    )
+)
+
+
+server <- function(input, output, session) {
+    
+    
+    # Define reactives ----
+    my_gene_id <- reactive({input$gene_id_or_name |>
+            split_text_to_vector() |>
+            convert_to_wb_id(gids)})
+    my_gene_id_1 <- reactive(my_gene_id()[[1]])
+    my_gene_name_1 <- reactive(i2s(my_gene_id_1(), gids))
+    my_neurons <- reactive(input$neurons |>
+                               str_to_upper() |>
+                               split_text_to_vector() |>
+                               validate_neurons(neurons_table))
+    
+    useColorScale <- reactive(accepted_color_scales$ggplot[accepted_color_scales$shortcode==input$useColorScale])
+    
+    
+    
+    
+    
+    
+    # Single gene plots ----
+    output$tx_neuron_proportions <- renderPlotly({
+        
+        ind_graph <- tx_long |>
+            filter(gene_id == my_gene_id_1(),
+                   neuron_id %in% my_neurons()) |>
+            group_by(transcript_id, neuron_id) |>
+            summarize(mean_cnt = mean(TPM),
+                      sd_cnt = sd(TPM),
+                      .groups = "drop") |>
+            group_by(neuron_id) |>
+            mutate(prop = round(100*mean_cnt/sum(mean_cnt), 1),
+                   sd = sd_cnt/sum(mean_cnt)) |>
+            rename(Neuron = neuron_id,
+                   Transcript = transcript_id,
+                   `Transcript Usage` = prop) |>
+            ggplot(aes(x=Neuron,y=`Transcript Usage`, fill = Transcript)) +
+            geom_col(position = position_stack()) +
+            # geom_errorbar(aes(ymin=prop,ymax=prop+sd), position = position_stack()) +
+            theme_classic() +
+            coord_flip() +
+            labs(x=NULL,y="Mean proportion",title=paste0(my_gene_name_1(), "/", my_gene_id_1())) +
+            # guides(fill=guide_legend(title=my_gene_name_1())) +
+            scale_y_continuous(labels = pct) +
+            useColorScale()
+        
+        ggplotly(ind_graph)
+        
+    })
+    
+    
+    output$tx_neuron_tpm <- renderPlotly({
+        
+        ind_graph <- tx_long |>
+            filter(gene_id == my_gene_id_1(),
+                   neuron_id %in% my_neurons()) |>
+            group_by(transcript_id, neuron_id) |>
+            summarize(`Mean TPM` = mean(TPM),
+                      sd = sd(TPM),
+                      .groups = "drop") |>
+            rename(`Neuron` = neuron_id,
+                   `Transcript` = transcript_id) |>
+            ggplot(aes(x=`Neuron`, y=`Mean TPM`, color = `Transcript`)) +
+            geom_point(position=position_dodge(.2), pch=15,size=2) +
+            geom_errorbar(aes(ymin=`Mean TPM`-sd,ymax=`Mean TPM`+sd),width=0,position=position_dodge(.2)) +
+            theme_classic() +
+            useColorScale() +
+            coord_flip() +
+            {if(input$log_scale) scale_y_log10()} +
+            labs(x=NULL,y="Mean transcript TPM (+/-sd)",title=paste0(my_gene_name_1(), "/", my_gene_id_1()))
+        
+        ggplotly(ind_graph)
+        
+    })
+    
+    
+    
+    output$tx_samples_proportions <- renderPlotly({
+        
+        ind_graph <- tx_long |>
+            filter(gene_id == my_gene_id_1(),
+                   neuron_id %in% my_neurons()) |>
+            group_by(sample_id, gene_id) |>
+            mutate(sample_prop = round(100*TPM/sum(TPM), 1)) |>
+            rename(Neuron = neuron_id,
+                   Transcript = transcript_id,
+                   `Transcript Proportion` = sample_prop) |>
+            ggplot(aes(x=sample_id, y=`Transcript Proportion`, fill = Transcript)) +
+            geom_col(position = position_stack()) +
+            # geom_errorbar(aes(ymin=prop,ymax=prop+sd), position = position_stack()) +
+            theme_classic() +
+            coord_flip() +
+            labs(x=NULL,y="Proportion usage",title=paste0(my_gene_name_1(), "/", my_gene_id_1())) +
+            scale_y_continuous(labels = pct) +
+            useColorScale()
+        
+        ggplotly(ind_graph)
+        
+    })
+    
+    output$tx_samples_tpm <- renderPlotly({
+        
+        ind_graph <- tx_long |>
+            filter(gene_id == my_gene_id_1(),
+                   neuron_id %in% my_neurons()) |>
+            rename(Neuron = neuron_id,
+                   Transcript = transcript_id,
+                   `Transcript Usage` = TPM) |>
+            ggplot(aes(x=sample_id, y=`Transcript Usage`, fill = Transcript)) +
+            theme_classic() +
+            geom_point(position=position_dodge(.2), pch=15,size=2) +
+            coord_flip() +
+            {if(input$log_scale) scale_y_log10()} +
+            labs(x=NULL,y="Transcript TPM",title=paste0(my_gene_name_1(), "/", my_gene_id_1())) +
+            useColorScale()
+        
+        ggplotly(ind_graph)
+        
+    })
+    
+    
+    
+    # Heatmaps ----
+    # output$heatmapCplx <- renderPlot({
+    #     
+    #     message("Plot Cplx!")
+    #     tx_long |>
+    #         filter(gene_id %in% my_gene_id(),
+    #                neuron_id %in% my_neurons()) |>
+    #         mutate(gene_id = i2s(gene_id, gids)) |>
+    #         group_by(gene_id, transcript_id, neuron_id) |>
+    #         summarize(mean_tpm = mean(TPM)) |>
+    #         rename(TPM = mean_tpm,
+    #                Transcript = transcript_id,
+    #                Neuron = neuron_id,
+    #                Gene = gene_id) |>
+    #         group_by(Gene) |>
+    #         tidyHeatmap::heatmap(Transcript, Neuron, TPM,
+    #                              transform = log1p, .scale = "none",
+    #                              cluster_columns=FALSE,
+    #                              cluster_rows = FALSE,
+    #                              column_names_gp = grid::gpar(fontsize = 9))
+    #     
+    #     
+    # })
+    
+    output$heatmapPhmp <- renderPlot({
+
+        tx_long |>
+            filter(gene_id %in% my_gene_id(),
+                   neuron_id %in% my_neurons()) |>
+            mutate(gene_id = i2s(gene_id, gids)) |>
+            group_by(gene_id, transcript_id, neuron_id) |>
+            summarize(mean_tpm = log1p(mean(TPM)),
+                      .groups = "drop") |>
+            rename(`log(TPM)` = mean_tpm,
+                   Transcript = transcript_id,
+                   Neuron = neuron_id,
+                   Gene = gene_id) |>
+            mutate(Transcript = factor(Transcript, levels = sort(as.character(unique(Transcript))))) |>
+            arrange(Gene, Transcript, Neuron) |>
+            tidyheatmap::tidy_heatmap(rows = Transcript,
+                                      columns =  Neuron,
+                                      values =  `log(TPM)`,
+                                      annotation_row = Gene,
+                                      gaps_row = Gene,
+                                      fontsize = 9,
+                                      main = "Transcript expression level (log TPM — mean per neuron)")
+    })
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
